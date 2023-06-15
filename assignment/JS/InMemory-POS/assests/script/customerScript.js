af84d6dec0aad57deff86bf5cefdb01a815a8323
buddini96 <buddinikl1996@gmail.com>
2023-06-15 13:02:35 +0530

    updated

---
+++ assignment/JS/InMemory-POS/assests/script/customerScript.js
@@ -0,0 +1,102 @@
+
+    let customerList = [];
+    let customerIndex;
+
+
+    function clearCustomerFields() {
+    $('#txtcid').val("");
+    $('#txtName').val("");
+    $('#txtAddress').val("");
+    $('#txtcontact').val("");
+}
+
+
+    $("#btnSaveCustomer").click(function(){
+    let cusId = $("#txtcid").val();
+    let cusName = $("#txtName").val();
+    let cusAddress = $("#txtAddress").val();
+    let cusContact = $("#txtcontact").val();
+
+
+    let tBody = $("#tblCustomer");
+
+    let tr = $('<tr><td>'+cusId+'</td> <td>'+cusName+'</td> <td>'+cusAddress+'</td> <td>'+cusContact+'</td></tr>');
+
+    //set the row to the table body
+    tBody.append(tr);
+
+    customerList.push({id:cusId, name:cusName, address:cusAddress, contact:cusContact})
+    console.log(customerList)
+});
+
+
+
+    let selectedCustomerRow;
+
+    $("#tblCustomer").click(function (event){
+    selectedCustomerRow = event.target.closest('tr');
+    $('#txtcid').val(selectedCustomerRow.cells[0].textContent);
+    $('#txtName').val(selectedCustomerRow.cells[1].textContent);
+    $('#txtAddress').val(selectedCustomerRow.cells[2].textContent);
+    $('#txtcontact').val(selectedCustomerRow.cells[3].textContent);
+
+    customerIndex = customerList.findIndex(customerList => customerList.id === selectedCustomerRow.cells[0].textContent);
+
+});
+
+
+    $("#btnUpdateCustomer").click(function(){
+    console.log("Ebuwa");
+    let cusId = $("#txtcid").val();
+    let cusName = $("#txtName").val();
+    let cusAddress = $("#txtAddress").val();
+    let cusContact = $("#txtcontact").val();
+
+    /*console.log(cusId,cusName,cusAddress,cusContact);*/
+    console.log(selectedCustomerRow);
+    selectedCustomerRow.cells[0].textContent=cusId;
+    selectedCustomerRow.cells[1].textContent=cusName;
+    selectedCustomerRow.cells[2].textContent=cusAddress;
+    selectedCustomerRow.cells[3].textContent=cusContact;
+
+    clearCustomerFields();
+    console.log(customerIndex);
+
+
+    //updating the selected customer from the list
+    customerList[customerIndex].id=cusId;
+    customerList[customerIndex].name=cusName;
+    customerList[customerIndex].address=cusAddress;
+    customerList[customerIndex].contact=cusContact;
+
+    console.log(customerList);
+
+    /*btnUpdate.prop('disabled',true);
+    btnDelete.prop('disabled',true);
+    btnAdd.prop('disabled',false);*/
+});
+
+
+
+    $("#btnDeleteCustomer").click(function(){
+    console.log("hhh");
+    selectedCustomerRow.remove();
+
+    //removing the selected customer from the list
+    customerList.splice(customerIndex,1);
+    console.log(customerList);
+
+    clearCustomerFields();
+    console.log(customerList);
+    /*btnUpdate.prop('disabled',true);
+    btnDelete.prop('disabled',true);
+    btnSave.prop('disabled',false);*/
+});
+
+
+
+    $("#btnClearCustomer").click(function(){
+    clearCustomerFields();
+})
+
+
---
+++ assignment/JS/InMemory-POS/assests/script/itemScript.js
@@ -0,0 +1,103 @@
+
+
+let itemList = [];
+let itemIndex;
+
+function clearItemFields() {
+    $('#txtItemId').val("");
+    $('#txtItemName').val("");
+    $('#txtItemPrice').val("");
+    $('#txtItemQty').val("");
+}
+
+$("#btnSaveItem").click(function(){
+    let itemId = $("#txtItemId").val();
+    let itemName = $("#txtItemName").val();
+    let itemPrice = $("#txtItemPrice").val();
+    let itemQty = $("#txtItemQty").val();
+
+
+    let tBody = $("#tblItem");
+
+    let tr = $('<tr><td>'+itemId+'</td> <td>'+itemName+'</td> <td>'+itemPrice+'</td> <td>'+itemQty+'</td></tr>');
+
+    //set the row to the table body
+    tBody.append(tr);
+
+    itemList.push({id:itemId, name:itemName, price:itemPrice, qty:itemQty})
+    console.log(itemList)
+});
+
+
+let selectedItemRow;
+
+$("#tblItem").click(function (event){
+    selectedItemRow = event.target.closest('tr');
+    console.log(selectedItemRow.cells[0]);
+    $('#txtItemId').val(selectedItemRow.cells[0].textContent);
+    $('#txtItemName').val(selectedItemRow.cells[1].textContent);
+    $('#txtItemPrice').val(selectedItemRow.cells[2].textContent);
+    $('#txtItemQty').val(selectedItemRow.cells[3].textContent);
+
+    itemIndex = itemList.findIndex(itemList => itemList.id === selectedItemRow.cells[0].textContent);
+
+});
+
+
+
+
+$("#btnUpdateItem").click(function(){
+
+    let itemId = $("#txtItemId").val();
+    let itemName = $("#txtItemName").val();
+    let itemPrice = $("#txtItemPrice").val();
+    let itemQty = $("#txtItemQty").val();
+
+    /*console.log(cusId,cusName,cusAddress,cusContact);*/
+    console.log(selectedItemRow);
+    selectedItemRow.cells[0].textContent=itemId;
+    selectedItemRow.cells[1].textContent=itemName;
+    selectedItemRow.cells[2].textContent=itemPrice;
+    selectedItemRow.cells[3].textContent=itemQty;
+
+    clearItemFields();
+    console.log(itemIndex);
+
+
+    //updating the selected customer from the list
+    itemList[itemIndex].id=itemId;
+    itemList[itemIndex].name=itemName;
+    itemList[itemIndex].price=itemPrice;
+    itemList[itemIndex].qty=itemQty;
+
+    console.log(itemList);
+
+    /*btnUpdate.prop('disabled',true);
+    btnDelete.prop('disabled',true);
+    btnAdd.prop('disabled',false);*/
+});
+
+
+$("#btnDeleteItem").click(function(){
+    console.log("hhh");
+    selectedItemRow.remove();
+
+    //removing the selected customer from the list
+    itemList.splice(itemIndex,1);
+    console.log(itemList);
+
+    clearItemFields();
+    console.log(itemList);
+    /*btnUpdate.prop('disabled',true);
+    btnDelete.prop('disabled',true);
+    btnSave.prop('disabled',false);*/
+});
+
+
+
+$("#btnClearItem").click(function(){
+    clearItemFields();
+})
+
+
+
